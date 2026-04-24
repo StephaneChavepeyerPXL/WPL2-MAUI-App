d96d674256bb09245e675f8d909fcde583cf280e
Stephane Chavepeyer <stephane.chavepeyer@student.pxl.be>
2026-04-24 16:04:27 +0200

    CL-02: SQL script toegevoegd van de gemaakte database & tabel

---
+++ SteCvp.Infrastructure/Database/CreatePokemonCardsTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE dbo.PokemonCards (
+                                  Id INT IDENTITY(1,1) PRIMARY KEY,
+                                  Name NVARCHAR(50) NOT NULL,
+                                  SetName NVARCHAR(50) NOT NULL,
+                                  Rarity NVARCHAR(25) NOT NULL,
+                                  EstimatedValue DECIMAL(10,2) NOT NULL,
+                                  PhotoUrl NVARCHAR(500) NOT NULL
+);
