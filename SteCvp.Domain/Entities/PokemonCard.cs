fd9f99e27bdc0a1eed9811942143ed496fe0d591
Stephane Chavepeyer <stephane.chavepeyer@student.pxl.be>
2026-04-24 15:09:13 +0200

    CL-01: PokemonCard entiteit toegevoegd in Domain met properties

--- SteCvp.Domain/Entities/PokemonCard.cs
+++ SteCvp.Domain/Entities/PokemonCard.cs
@@ -8,6 +8,11 @@ namespace SteCvp.Domain.Entities
 {
     public class PokemonCard
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SetName { get; set; }
+        public string Rarity { get; set; }
+        public decimal EstimatedValue { get; set; }
+        public string PhotoUrl { get; set; }
     }
 }
