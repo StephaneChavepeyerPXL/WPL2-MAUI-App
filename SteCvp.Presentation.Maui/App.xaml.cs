ce100161eb8aaf4e618b9fdf738b84f357047dc9
Stephane Chavepeyer <stephane.chavepeyer@student.pxl.be>
2026-04-27 14:00:54 +0200

    Maui project toegevoegd

---
+++ SteCvp.Presentation.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SteCvp.Presentation.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SteCvp.Presentation.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SteCvp.Presentation.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
