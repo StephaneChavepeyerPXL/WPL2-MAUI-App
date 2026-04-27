e45efb533041aa201e368d71b0b14cbeb81890c3
Stephane Chavepeyer <stephane.chavepeyer@student.pxl.be>
2026-04-27 14:33:13 +0200

    CL-03: GetAllPokemonCards methode toegevoegd in PokemonCardService

---
+++ SteCvp.Application/Interfaces/IPokemonCardRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteCvp.Domain.Entities;
+
+namespace SteCvp.Application.Interfaces
+{
+    public interface IPokemonCardRepository
+    {
+        Task<IEnumerable<PokemonCard>> GetAllAsync();
+    }
+}
---
+++ SteCvp.Application/Services/PokemonCardService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteCvp.Application.Interfaces;
+using SteCvp.Domain.Entities;
+
+namespace SteCvp.Application.Services
+{
+    public class PokemonCardService
+    {
+        private readonly IPokemonCardRepository _pokemonCardRepository;
+
+        public PokemonCardService(IPokemonCardRepository pokemonCardRepository)
+        {
+            _pokemonCardRepository = pokemonCardRepository;
+        }
+
+        public Task<IEnumerable<PokemonCard>> GetAllPokemonCards()
+        {
+            return _pokemonCardRepository.GetAllAsync();
+        }
+    }
+}
