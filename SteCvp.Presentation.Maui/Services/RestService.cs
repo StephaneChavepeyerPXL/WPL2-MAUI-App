6f3abb1806d3479a5fe4a30f986e510f2a4331ff
Stephane Chavepeyer <stephane.chavepeyer@student.pxl.be>
2026-05-09 11:24:19 +0200

    MAUI-05: RestService toegevoegd met HttpClient

---
+++ SteCvp.Presentation.Maui/Services/RestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteCvp.Presentation.Maui.Services
+{
+    public class RestService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RestService()
+        {
+            _httpClient = new HttpClient();
+        }
+    }
+}
